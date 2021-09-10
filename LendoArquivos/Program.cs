dc1257b6e59e3c166f563cf7012277a7c3662d8d
Monteirogui1 <guimonteiro8@gmail.com>
2021-09-10 14:18:01 -0300

    Adicionar arquivos de projeto.

---
+++ LendoArquivos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LendoArquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arquivo = @"C:/Users/HeadThink/Desktop/Login.txt";
+            if (File.Exists(arquivo))
+            {
+                try
+                {
+                    using (StreamReader sr = new StreamReader(arquivo))
+                    {
+                        String linha;
+                        // Lê linha por linha até o final do arquivo
+                        while ((linha = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(linha);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine(" O arquivo " + arquivo + "não foi localizado !");
+            }
+            Console.ReadKey();
+        }
+    }
+}
